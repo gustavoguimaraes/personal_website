cf6593d65ce4698655f7e3417d5792be6b716f8b
Gustavo <gustavoguimaraes@gmail.com>
2014-01-06 13:08:17 -0500

    Add Jquery to the portfolio

--- app.rb
+++ app.rb
@@ -4,23 +4,6 @@ Bundler.require
 class Portifolio < Sinatra::Application
   
   get '/' do
-    erb :index
+    erb :layout
   end
-
-  get '/about' do 
-    erb :about
-  end
-
-  get '/links' do 
-    erb :links
-  end
-
-  get '/projects' do 
-    erb :projects
-  end
-
-  get '/contact' do 
-    erb :contact
-  end
-
 end
